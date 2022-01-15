a10a100452dd3b643a63d3762abf6b18f8ce45b7
kishanmund <kishan.mund@gmail.com>
2022-01-15 19:40:57 +0530

    Responsive

--- main.js
+++ main.js
@@ -2,21 +2,51 @@ let toggle=true
 
 let user=true
 
-function sideBar() {
-    if(toggle){
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("container").style.marginLeft = "250px";
-        
+
+
+function sideBar(i) {
+    if(i!='mob'){
+        if(toggle){
+            document.getElementById("mySidebar").style.width = "250px";
+            document.getElementById("container").style.marginLeft = "250px";
+            
+        }
+        else{
+            document.getElementById("mySidebar").style.width = "0";
+            document.getElementById("container").style.marginLeft= "0";
+        }
+        toggle=!toggle
     }
     else{
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("container").style.marginLeft= "0";
+        if(toggle){
+            document.getElementById("mySidebar").style.width = "100%";
+            document.getElementById("container").style.marginLeft = "0";
+            
+        }
+        else{
+            document.getElementById("mySidebar").style.width = "0";
+            document.getElementById("container").style.marginLeft= "0";
+        }
+        toggle=!toggle
     }
-    toggle=!toggle
   }
 
 function url(name){
-    window.history.pushState('page2', 'Title', '/index.html?users');
+    if(name == "table"){
+        document.getElementById("breadcrumbs").innerHTML="All Users"
+        window.history.pushState('page2', 'Title', '/index.html?users');
+        document.getElementById("users").style.display="none"
+        document.getElementById("table").style.display="block"
+    }
+    else{
+        {
+            document.getElementById("breadcrumbs").innerHTML="Manage User"
+            window.history.pushState('page2', 'Title', '/index.html?addUser');
+            document.getElementById("users").style.display="block"
+            document.getElementById("table").style.display="none"
+        }
+    }
+    
 }
 
 
